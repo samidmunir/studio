935f9bb69169d219865ddeb3377032043b9efa6c
Sami Munir <sami.munir2001@gmail.com>
2025-05-29 15:14:34 -0400

    establishing MongoDB conn with server

--- Projects/career-dock/server/index.js
+++ Projects/career-dock/server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+// import userRouter from "./routes/user.route.js";
+// import recordRouter from "./routes/record.route.js";
 
 dotenv.config();
 
@@ -11,7 +14,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.use("/api/user", userRouter);
+// app.use("/api/record", recordRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}...`);
   console.log(`JWT_SECRET: ${process.env.JWT_SECRET}`);
+  connectDB();
 });
