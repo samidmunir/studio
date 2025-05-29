79a864d8f20abfe37a80ad55b5690b220caa3240
Sami Munir <sami.munir2001@gmail.com>
2025-05-29 15:29:39 -0400

    scaffolding client vite + tailwind + typescript

---
+++ Projects/career-dock/client/src/App.tsx
@@ -0,0 +1,54 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Routes, Route, useLocation } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import CreateProductPage from "./pages/CreateProductPage";
+import EditProductPage from "./pages/EditProductPage";
+import Protected from "./components/Protected";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <Navbar />
+      <ToastContainer position="top-right" />
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Protected>
+              <DashboardPage />
+            </Protected>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/create"
+          element={
+            <Protected>
+              <CreateProductPage />
+            </Protected>
+          }
+        />
+        <Route
+          path="/record/edit/:id"
+          element={
+            <Protected>
+              <EditProductPage />
+            </Protected>
+          }
+        />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ Projects/career-dock/client/src/context/UserContext.tsx
@@ -0,0 +1,71 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  type ReactNode,
+  useEffect,
+} from "react";
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: "admin" | "user";
+};
+
+interface UserContextType {
+  user: User | null;
+  token: string | null;
+  login: (token: string, user: User) => void;
+  logout: () => void;
+  loading: boolean;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+    if (storedToken && storedUser) {
+      setToken(storedToken);
+      setUser(JSON.parse(storedUser));
+    }
+
+    setLoading(false);
+  }, []);
+
+  const login = (token: string, user: User) => {
+    setToken(token);
+    setUser(user);
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <UserContext.Provider value={{ user, token, login, logout, loading }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within an UserProvider");
+  }
+
+  return context;
+};
