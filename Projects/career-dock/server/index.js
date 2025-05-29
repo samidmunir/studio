ca30f10cf10b18dc0bf50c026e91944ebe494787
Sami Munir <sami.munir2001@gmail.com>
2025-05-29 15:18:50 -0400

    implementing user router + record router

--- Projects/career-dock/server/index.js
+++ Projects/career-dock/server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-// import userRouter from "./routes/user.route.js";
-// import recordRouter from "./routes/record.route.js";
+import userRouter from "./routes/user.route.js";
+import recordRouter from "./routes/record.route.js";
 
 dotenv.config();
 
@@ -14,8 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/user", userRouter);
-// app.use("/api/record", recordRouter);
+app.use("/api/user", userRouter);
+app.use("/api/record", recordRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}...`);
